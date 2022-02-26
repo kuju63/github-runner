5cd0a2a8cadf555c65897367eae315c55294c42f
Jun Kurihara <lh182051+src@gmail.com>
2022-02-26 14:53:50 +0000

    :sparkles: support tls no verify option

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export TOKEN="${token}"
+
 if [ -z "${token}"  ]; then
   if [ -z "${token_file}" ]; then
     echo "token must be set" 1>&2
@@ -8,10 +10,16 @@ if [ -z "${token}"  ]; then
     echo "Token file does not found" 1>&2
     exit 1
   else
-    export TOKEN=$(cat $token_file)
+    secret_token="$(cat "$token_file")"
+    export TOKEN="$secret_token"
   fi
-else
-  export TOKEN="${token}"
+fi
+
+if [ -z "${tls_no_verify}" ]; then
+  export GITHUB_ACTIONS_RUNNER_TLS_NO_VERIFY=0
+elif [ "$tls_no_verify" = "1" ]; then
+echo "Disabled TLS verify check"
+  export GITHUB_ACTIONS_RUNNER_TLS_NO_VERIFY=1
 fi
 
 if [ ! -f "./.credentials" ]; then
@@ -20,12 +28,12 @@ fi
 
 shutdown_handler() {
     echo "Start shutdown process"
-    ./config.sh remove --token $TOKEN
+    ./config.sh remove --token "$TOKEN"
 }
 
 ./run.sh &
 pid="$!"
-trap "shutdown_handler $pid" SIGTERM
+trap '"shutdown_handler $pid"' SIGTERM
 while kill -0 $pid > /dev/null 2>&1; do
     wait
 done
